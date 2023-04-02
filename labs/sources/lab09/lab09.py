c8ae1faf631287b32e44021c9e0a26ef15f3c18d
Dominik Vranay <47859587+DominikVranay@users.noreply.github.com>
2023-04-02 19:43:06 +0200

    Update lab09.py

--- labs/sources/lab09/lab09.py
+++ labs/sources/lab09/lab09.py
@@ -1,8 +1,7 @@
-from keras.layers import Input, Dense
-from keras.models import Model
-from keras.datasets import mnist
-import numpy as np
+import torch
+import torchvision
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_results(x_test, encoded_imgs, decoded_imgs, n=10):
@@ -10,7 +9,7 @@ def plot_results(x_test, encoded_imgs, decoded_imgs, n=10):
     for i in range(n):
         # display original
         ax = plt.subplot(3, n, i + 1)
-        plt.imshow(x_test[i].reshape(28, 28))
+        plt.imshow(x_test[i])
         plt.gray()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
@@ -24,38 +23,80 @@ def plot_results(x_test, encoded_imgs, decoded_imgs, n=10):
 
         # display reconstruction
         ax = plt.subplot(3, n, i + 1 + n * 2)
-        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.imshow(decoded_imgs[i])
         plt.gray()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
     plt.show()
 
 
-(mnist_train, _), (mnist_test, _) = mnist.load_data()
+trainset = torchvision.datasets.MNIST('/files/', train=True, download=True, transform=torchvision.transforms.ToTensor())
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True)
+testset = torchvision.datasets.MNIST('/files/', train=True, download=True, transform=torchvision.transforms.ToTensor())
+testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False)
 
-x_train =  # TODO: normalize data to interval 0-1
-x_test =  # TODO: normalize data to interval 0-1
-x_train =  # TODO: reshape to flattened input: (len, 28, 28) -> (len, 784)
-x_test =  # TODO: reshape to flattened input: (len, 28, 28) -> (len, 784)
+encoder = torch.nn.Sequential(torch.nn.Flatten) # pridať vrstvy
+class Decoder(torch.nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.all = torch.nn.Sequential() # pridať vrstvy
+        
+    
+    def forward(self, x):
+        x = self.all(x).view(-1, 28, 28)
+        return x
+decoder = Decoder()
 
-input_img =  # TODO: define input layer with size
+class AutoEncoder(torch.nn.Module):
+    def __init__(self, encoder, decoder):
+        super(AutoEncoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
 
-encoded =  # TODO: define hidden layer - 32 neurons, relu; connect to input
-decoded =  # TODO: define output layer, same as input, use sigmoid act
+model = AutoEncoder(encoder, decoder)
 
-autoencoder =  # TODO: define entire model
-encoder =  # TODO: define encoder part
+optimizer = None
+criterion = None
+for epoch in range(100):
+    ls = []
+    for inputs, _ in trainloader:
+        optimizer.zero_grad()
+        preds = model(inputs)
+        loss = criterion(preds, inputs)
+        loss.backward()
+        optimizer.step()
+        ls.append(loss.detach().item())
+    print("Loss:", sum(ls)/len(ls))
 
-encoded_input =  # TODO: define new input layer for encoded code
-decoder_layer =  # TODO: load last autoencoder layer, connect with previous
-decoder =  # TODO: define decoder model
+inputs, _ = next(testloader)
+encoded_imgs = encoder(inputs)
+decoded_imgs = decoder(encoded_imgs)
 
-autoencoder.compile(  # TODO: add parameters
-)
-autoencoder.fit(  # TODO: add parameters
-)
+plot_results(inputs, encoded_imgs, decoded_imgs)
 
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = autoencoder.predict(x_test)
+def plot_results(encoded_imgs, decoded_imgs, n=10):
+    plt.figure(figsize=(40, 4))
+    for i in range(n):
+        # display encoded
+        ax = plt.subplot(3, n, i + 1)
+        plt.imshow(encoded_imgs[i].reshape(8, 4))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-plot_results(x_test, encoded_imgs, decoded_imgs)
+        # display reconstruction
+        ax = plt.subplot(3, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i])
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+inpu = np.random.random((300, 32))
+decoded_imgs = decoder(inpu)
+plot_results(inpu, decoded_imgs)
